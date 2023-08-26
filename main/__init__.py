2064f4bf612e6105f38c6d09ae851afa46dd29cf
mobn0 <108767654+mobn0@users.noreply.github.com>
2023-08-26 18:23:50 +0000

    d

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,11 @@
 import pygame
 import screeninfo as si
+import tkinter
 
+
+root = tkinter.Tk()
+root.withdraw()
+WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
 for x in si.get_monitors():
     print(x)
 
