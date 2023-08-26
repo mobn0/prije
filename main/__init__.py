57294335a3ff7014f87fca273174f120afdc86fa
mobn0 <108767654+mobn0@users.noreply.github.com>
2023-08-26 16:08:44 +0000

    h

--- main/__init__.py
+++ main/__init__.py
@@ -1,11 +1,8 @@
 import pygame
 import screeninfo as si
-<<<<<<< HEAD
 print("lopöpökpiolüäöüäpüüpö")
 print("ho")
 x = 10
 x += 101
 go = x+x
-=======
 print("lopöpökpiolüäöüäpüüpö")
->>>>>>> 74cc80bb6b52b19fbef8ba9c7107147f25b524c9
